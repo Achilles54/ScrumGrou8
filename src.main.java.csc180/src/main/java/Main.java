3d0ba25918e06cd1baca7cde5f3b54ee2db6d26f
adasilva <adasilva@student.neumont.edu>
2025-02-18 15:55:22 -0700

    Began Console UX Framework

--- src.main.java.csc180/src/main/java/Main.java
+++ src.main.java.csc180/src/main/java/Main.java
@@ -1,22 +1,51 @@
 import Model.EventDay;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Main {
+    static ArrayList<EventDay> calander = new ArrayList<>();
+
     public static void main(String[] args) {
-        ArrayList<EventDay> calander = new ArrayList<>();
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("""
-                Welcome to the Calander Project (Console Implementation
-                1. View Events
-                2. Create New Event
-                3. Edit Event
-                4. Quit""");
-        try{
-            String response = br.readLine();
+        boolean run = true;
+        while(run) {
+            System.out.println("""
+                    Welcome to the Calander Project (Console Implementation
+                    1. View Events
+                    2. Create New Event
+                    3. Edit Event
+                    4. Quit""");
+            try {
+                String response = br.readLine();
+                int numResponse = Integer.parseInt(response);
+                switch(numResponse){
+                    case 1:
+                        viewEvents();
+                        break;
+                    case 2:
+                        createEvent();
+                        break;
+                    case 3:
+                        editEvent();
+                        break;
+                    case 4:
+                        run = false;
+                        break;
+                    default:
+                        System.out.println("Invalid response");
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid Response, reattempt menu");
+            }
         }
-        switch()
+
+    }
+    public static void viewEvents() {
+
     }
 }
