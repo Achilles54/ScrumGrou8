038aeb8d8301644a18a1dd8449ce39c26d4c86ac
adasilva <adasilva@student.neumont.edu>
2025-02-19 16:06:32 -0700

    CreateEvent Function Work

--- src.main.java.csc180/src/main/java/Main.java
+++ src.main.java.csc180/src/main/java/Main.java
@@ -1,16 +1,21 @@
 import Model.EventDay;
+import Model.Months;
+import jdk.jfr.Event;
 
 import java.io.BufferedReader;
+import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.Month;
 import java.util.ArrayList;
 
 public class Main {
-    static ArrayList<EventDay> calander = new ArrayList<>();
+    static EventDay[] calander = new EventDay[365];
+    static Months[] months = new Months[]{Months.JANUARY,Months.FEBURARY,Months.MARCH,Months.APRIL,Months.MAY,Months.JUNE,Months.JULY,Months.AUGUST,Months.SEPTEMBER,Months.OCTOBER,Months.NOVEMBER,Months.DECEMBER};
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         boolean run = true;
         while(run) {
             System.out.println("""
@@ -46,6 +51,98 @@ public class Main {
 
     }
     public static void viewEvents() {
+        Months currentMonth = Months.DECEMBER;
+        for (int i = 0; i < 366; i++) {
+            if (calander[i] != null){
+                if(calander[i].getMonth() != currentMonth){
+                    System.out.println(calander[i].getMonth() + ": ");
+                    currentMonth = calander[i].getMonth();
+                }
+                switch(calander[i].getDay()%10){
+                    case 1:
+                        System.out.println(calander[i].getWeekday() + " the "
+                                + calander[i].getDay() + "st: " + calander[i].getEvent());
+                        break;
+                    case 2:
+                        System.out.println(calander[i].getWeekday() + " the "
+                                + calander[i].getDay() + "nd: " + calander[i].getEvent());
+                        break;
+                    case 3:
+                        System.out.println(calander[i].getWeekday() + " the "
+                                + calander[i].getDay() + "rd: " + calander[i].getEvent());
+                        break;
+                    case 4,5,6,7,8,9,0:
+                        System.out.println(calander[i].getWeekday() + " the "
+                                + calander[i].getDay() + "th: " + calander[i].getEvent());
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+    public static void createEvent() {
+        System.out.println("Please Enter Month of Event in numerical value (january = 1, feburary = 2, etc.)");
+        try {
+            String userResponse = br.readLine();
+            int numResponse = Integer.parseInt(userResponse);
+            if(numResponse >= 1 && numResponse <= months.length){
+                Months month = months[numResponse-1];
+            }
+            System.out.println("Please Enter Day of Event in numerical value");
+            System.out.println();
+            userResponse = br.readLine();
+            int day = Integer.parseInt(userResponse);
+            System.out.println("Please enter the Name of your event");
+            String event = br.readLine();
+            System.out.println("Please enter the description of or notes relating to your event");
+            String note = br.readLine();
+            int totalDays;
+            switch(numResponse){
+                case 1:
+                    totalDays = 0;
+                    break;
+                case 2:
+                    totalDays = 31;
+                    break;
+                case 3:
+                    totalDays = 59;
+                    break;
+                case 4:
+                    totalDays = 90;
+                    break;
+                case 5:
+                    totalDays = 120;
+                    break;
+                case 6:
+                    totalDays = 151;
+                    break;
+                case 7:
+                    totalDays = 181;
+                    break;
+                case 8:
+                    totalDays = 212;
+                    break;
+                case 9:
+                    totalDays = 243;
+                    break;
+                case 10:
+                    totalDays = 273;
+                    break;
+                case 11:
+                    totalDays = 304;
+                    break;
+                case 12:
+                    totalDays = 334;
+                    break;
+                default:
+                    totalDays = 0;
+            }
+            calander[totalDays + day] = new EventDay();
 
+        }catch(Exception e){
+            System.out.println("Invalid data");
+            createEvent();
+        }
     }
 }
--- src.main.java.csc180/src/main/java/Model/EventDay.java
+++ src.main.java.csc180/src/main/java/Model/EventDay.java
@@ -6,52 +6,43 @@ public class EventDay {
     private static Months[] months = {Months.JANUARY,Months.FEBURARY,Months.MARCH,Months.APRIL,Months.MAY,Months.JUNE,Months.JULY,Months.AUGUST,Months.SEPTEMBER,Months.OCTOBER,Months.NOVEMBER,Months.DECEMBER,Months.JANUARY,Months.FEBURARY};
     private int day;
     private Months month;
-    private int year;
     private Weekday weekday;
     private String event;
     private String note;
 
     //IMPORTANT NOTE: JANUARY IS 13 AND FEBURARY IS 14 for the Integer Month value
     //passed into this constructor. This is to make the weekday formula work.
-    public EventDay(int day, int month, int year, String event, String note) {
+    public EventDay(int day, int month, String event, String note) {
 
         setMonth(months[month]);
-        setYear(year);
+
         //Validation Check
         switch(this.month){
-            case Months.JANUARY,MARCH,MAY,JULY,AUGUST,OCTOBER,DECEMBER:
+            case JANUARY,MARCH,MAY,JULY,AUGUST,OCTOBER,DECEMBER:
                 if(day >= 1 && day <= 31){
                     this.day = day;
                 }else{
                     this.day = -1;
                 }
                 break;
-            case Months.APRIL,JUNE,SEPTEMBER,NOVEMBER:
+            case APRIL,JUNE,SEPTEMBER,NOVEMBER:
                 if(day >= 1 && day <= 30){
                     this.day = day;
                 }else{
                     this.day = -1;
                 }
-            case Months.FEBURARY:
-                if(year%4 == 0){
-                    if(day >= 1 && day <= 29){
-                        this.day = day;
-                    }else{
-                        this.day = -1;
-                    }
-                }else{
+            case FEBURARY:
                     if(day >= 1 && day <= 28){
                         this.day = day;
                     }else{
                         this.day = -1;
                     }
-                }
                 break;
         }
         setEvent(event);
         setNote(note);
         //COMPLICATED FORMULA USED TO FIND THE DAY OF THE WEEK OF ANY DATE
-        setWeekday(weekdays[(day+((13*(month + 1))/5) + (year%100) + ((year%100)/4) + (((year-(year%100))/100)/4)-2*20)%7]);
+        setWeekday(weekdays[(day+((13*(month + 1))/5) + (2025%100) + ((2025%100)/4) + (((2025-(2025%100))/100)/4)-2*20)%7]);
     }
 
     private void setDay(int day){
@@ -65,12 +56,6 @@ public class EventDay {
     }
     public Months getMonth(){
         return month;
-    }
-    private void setYear(int year){
-        this.year = year;
-    }
-    public int getYear(){
-        return year;
     }
     private void setEvent(String event){
         this.event = event;
