1eb9d02dfdf781ca96fad741803ecb854a859c71
Khayden Roberts <Khaydenroberts22@gmail.com>
2025-02-19 14:42:43 -0700

    UI finish

---
+++ src.main.java.csc180/src/main/java/clacController.java
@@ -0,0 +1,142 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class clacController {
+
+    @FXML
+    private Label Friday;
+
+    @FXML
+    private Label Monday;
+
+    @FXML
+    private Label Saturday;
+
+    @FXML
+    private Label Sunday;
+
+    @FXML
+    private Label Thursday;
+
+    @FXML
+    private Label Tuesday;
+
+    @FXML
+    private Label Wednesday;
+
+    @FXML
+    private Button createEvent;
+
+    @FXML
+    private Label dateBox1;
+
+    @FXML
+    private Label dateBox10;
+
+    @FXML
+    private Label dateBox11;
+
+    @FXML
+    private Label dateBox12;
+
+    @FXML
+    private Label dateBox13;
+
+    @FXML
+    private Label dateBox14;
+
+    @FXML
+    private Label dateBox15;
+
+    @FXML
+    private Label dateBox16;
+
+    @FXML
+    private Label dateBox17;
+
+    @FXML
+    private Label dateBox18;
+
+    @FXML
+    private Label dateBox19;
+
+    @FXML
+    private Label dateBox2;
+
+    @FXML
+    private Label dateBox20;
+
+    @FXML
+    private Label dateBox21;
+
+    @FXML
+    private Label dateBox22;
+
+    @FXML
+    private Label dateBox23;
+
+    @FXML
+    private Label dateBox24;
+
+    @FXML
+    private Label dateBox25;
+
+    @FXML
+    private Label dateBox26;
+
+    @FXML
+    private Label dateBox27;
+
+    @FXML
+    private Label dateBox28;
+
+    @FXML
+    private Label dateBox29;
+
+    @FXML
+    private Label dateBox3;
+
+    @FXML
+    private Label dateBox30;
+
+    @FXML
+    private Label dateBox31;
+
+    @FXML
+    private Label dateBox32;
+
+    @FXML
+    private Label dateBox33;
+
+    @FXML
+    private Label dateBox34;
+
+    @FXML
+    private Label dateBox35;
+
+    @FXML
+    private Label dateBox4;
+
+    @FXML
+    private Label dateBox5;
+
+    @FXML
+    private Label dateBox6;
+
+    @FXML
+    private Label dateBox7;
+
+    @FXML
+    private Label dateBox8;
+
+    @FXML
+    private Label dateBox9;
+
+    @FXML
+    private Button left;
+
+    @FXML
+    private Button right;
+
+}
